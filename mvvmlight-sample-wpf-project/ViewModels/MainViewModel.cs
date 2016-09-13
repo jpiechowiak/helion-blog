51d0db4e8fb2963815fdfda128a870a5a8c4fb7b
Jerzy Piechowiak <jerzy.piechowiak@gmail.com>
2016-09-13 20:22:46 +0200

    mvvmlight-sample-wpf-project

---
+++ mvvmlight-sample-wpf-project/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+
+namespace MvvmLightSampleWPFProject.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private Guid data;
+
+        public MainViewModel()
+        {
+            this.LoadDataCommand = new RelayCommand(() =>
+            {
+                this.Data = Guid.NewGuid();
+            });
+        }
+
+        public ICommand LoadDataCommand { get; private set; }
+
+        public Guid Data
+        {
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+                this.RaisePropertyChanged("Data");
+            }
+        }
+    }
+}
---
+++ mvvmlight-sample-wpf-project/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace MvvmLightSampleWPFProject.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
